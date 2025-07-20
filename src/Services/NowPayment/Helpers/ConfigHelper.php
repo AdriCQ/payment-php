dd66c6f104cc64da7fab11a135eebc573d530a9d
AdriCQ <adriancapote95@gmail.com>
2025-07-20 13:45:42 -0400

    chore(now-payment): add default fallback values to ConfigHelper methods

--- src/Services/NowPayment/Helpers/ConfigHelper.php
+++ src/Services/NowPayment/Helpers/ConfigHelper.php
@@ -6,41 +6,41 @@ final class ConfigHelper
 {
     public static function paymentCurrency(): string
     {
-        return config('payment.now_payments.currency');
+        return config('payment.now_payments.currency', 'usdttrc20');
     }
 
     public static function webhookFullUrl(): string
     {
-        return config('app.url') . self::webhookUrl();
+        return config('app.url', 'http://localhost:8000') . self::webhookUrl();
     }
 
     public static function webhookUrl(): string
     {
-        return config('payment.now_payments.webhook.url');
+        return config('payment.now_payments.webhook.url', '/api/now-payment/webhook');
     }
 
     public static function apiUrl(): string
     {
-        return config('payment.now_payments.url');
+        return config('payment.now_payments.url', 'https://api.nowpayments.io');
     }
 
     public static function apiKey(): string
     {
-        return config('payment.now_payments.secret_token');
+        return config('payment.now_payments.secret_token', 'secret_token');
     }
 
     public static function successInvoiceUrl(): string
     {
-        return config('payment.now_payments.invoice.success_url');
+        return config('payment.now_payments.invoice.success_url', 'http://localhost:8000');
     }
 
     public static function cancelInvoiceUrl(): string
     {
-        return config('payment.now_payments.invoice.cancel_url');
+        return config('payment.now_payments.invoice.cancel_url', 'http://localhost:8000');
     }
 
     public static function webhookToken(): string
     {
-        return config('payment.now_payment.webhook.token');
+        return config('payment.now_payment.webhook.token', 'token');
     }
 }
